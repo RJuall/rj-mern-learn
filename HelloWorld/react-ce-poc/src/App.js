ad9286007bd71aa8860f49c1c4d5e4738b48f2f6
Robert Juall <RJuall@gmail.com>
2019-02-06 10:39:42 -0500

    Trying to get Editor and Term working

--- HelloWorld/react-ce-poc/src/App.js
+++ HelloWorld/react-ce-poc/src/App.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+import brace from 'brace';
+import AceEditor from 'react-ace';
 import logo from './logo.svg';
+import 'brace/mode/haskell';
+import 'brace/theme/solarized_dark';
+import { XTerm, Terminal } from 'xterm';
+import { ResizableBox } from 'react-resizable';
+import * as throttle from 'lodash.throttle';
 import './App.css';
+import '../node_modules/xterm/dist/xterm.css';
+
+function onChange(newVal) {
+  console.log('change', newVal);
+}
+
+this.refs.xterm = Terminal;
 
 class App extends Component {
+  componentDidMount() {
+    runFakeTerminal(this.refs.xterm);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>HELLO WORLD</h1>
+        <div className="AceEditor">
+          <AceEditor
+            mode="haskell"
+            theme="solarized_dark"
+            onChange={onChange}
+            name="ace-ed"
+            editorProps={{$blockScrolling: true}}
+          />
+        </div>
+        <div id="terminal" className="XTerm">
+          <ResizableBox width={200} height={200} onResize={this.throttleConsoleResize} style={{
+              overflow: 'hidden'
+          }} >
+            <XTerm ref='xterm' style={{
+              addons:['fit', 'fullscreen', 'search'],
+              overflow: 'hidden',
+              position: 'relative',
+              width: '100%',
+              height: '100%'  
+            }} />
+          </ResizableBox>
+        </div>
       </div>
     );
   }
+}
+
+function runFakeTerminal(xterm) {
+  const term = xterm.getTerminal();
+  let shellprompt = '$';
+
+  function prompt() {
+    xterm.write('\r\n' + shellprompt);
+  };
+  xterm.writeln('');
 }
 
 export default App;
