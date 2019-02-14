72e88c25519cfbcc6dcc2fa888f0dd6af2960b12
Robert Juall <RJuall@gmail.com>
2019-02-13 21:35:38 -0500

    not getting very far with xterm

--- HelloWorld/xterm-test/src/App.js
+++ HelloWorld/xterm-test/src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
+import * as os from 'os';
+import * as pty from 'node-pty';
 import logo from './logo.svg';
 import './App.css';
+import { Terminal } from 'xterm';
+// import * as attach from 'xterm/lib/addons/attach/attach';
+import * as attach from '../addons/attach';
+import * as fit from 'xterm/lib/addons/fit/fit';
+import * as fullscreen from 'xterm/lib/addons/fullscreen/fullscreen';
+import * as search from 'xterm/lib/addons/search/search';
+import * as winptyCompat from 'xterm/lib/addons/winptyCompat/winptyCompat';
+
+Terminal.applyAddon(attach);
+Terminal.applyAddon(fit);
+Terminal.applyAddon(fullscreen);
+Terminal.applyAddon(search);
+Terminal.applyAddon(winptyCompat);
+
+const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+ 
       </div>
     );
   }
